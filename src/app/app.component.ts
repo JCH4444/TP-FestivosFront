a75e52b0a1a4b5d0a2f35daae60da259ff3a0961
JCH4444 <jhordyn00@gmail.com>
2024-06-04 17:16:38 -0500

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FestivosService } from './servicios/festivos.service';
+import { FestivoDto } from './entidades/festivo-dto';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Festivos';
+
+  public fechaSeleccionada: any;
+  public ano: number = new Date().getFullYear();
+  public columnas = [
+    { name: 'Festivo', prop: 'festivo' },
+    { name: 'Fecha', prop: 'fecha' },
+  ];
+  public festivos: FestivoDto[] = [];
+
+  public modoColumna = ColumnMode;
+  public tipoSeleccion = SelectionType;
+  public festivoSeleccion: FestivoDto | undefined;
+
+  constructor(
+    private festivosService: FestivosService,
+
+  ) {
+  }
+
+  public validarFecha() {
+    let fecha = new Date(this.fechaSeleccionada);
+    this.festivosService.verificarFecha(fecha).subscribe(
+      respuesta => {
+        window.alert(respuesta);
+      }
+    );
+  }
+
+  public obtenerFestivos() {
+    this.festivosService.obtenerFestivos(this.ano)
+      .subscribe(data => {
+        this.festivos = data;
+      },
+        err => {
+          window.alert(err.message)
+        });
+  }
+
+  public onActivate(event: any) {
+    if (event.type == 'click') {
+      this.festivoSeleccion = event.row;
+    }
+  }
+
+}
